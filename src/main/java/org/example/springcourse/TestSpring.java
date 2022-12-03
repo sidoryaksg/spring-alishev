6d4210ef7857f2cae2726e4dd1bb210e6ece2f05
Stanislav Sidoryak <stanislav.sidoryak@cma.ru>
2022-12-03 14:43:42 +0700

    Injecting dependencies

--- src/main/java/org/example/springcourse/TestSpring.java
+++ src/main/java/org/example/springcourse/TestSpring.java
@@ -8,16 +8,20 @@ public class TestSpring {
           "applicationContext.xml"
         );
 
-        Music rockMusic = context.getBean("rockMusic", Music.class);
-
-        MusicPlayer rockMusicPlayer = new MusicPlayer(rockMusic);
-        rockMusicPlayer.playMusic();
-
-        Music classicalMusic = context.getBean("classicalMusic", Music.class);
-
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(classicalMusic);
-        classicalMusicPlayer.playMusic();
-
+//        Music rockMusic = context.getBean("rockMusic", Music.class);
+//
+//        MusicPlayer rockMusicPlayer = new MusicPlayer(rockMusic);
+//        rockMusicPlayer.playMusic();
+//
+//        Music classicalMusic = context.getBean("classicalMusic", Music.class);
+//
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(classicalMusic);
+//        classicalMusicPlayer.playMusic();
+//
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
